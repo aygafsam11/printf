e524e92568f5627cdb7aabb5d07471379459f984
Gafar H Salam <aygafsam2020@gmail.com>
2022-10-16 13:01:43 -0700

    printf

---
+++ printf_rot13/_printarg.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _printarg - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _printarg(char *c)
+{
+	return (write(1, c, _strlen(c)));
+}
