c46b6a1f83fb380c23e07106c88b810150da4647
Gafar H. Salam <67158388+aygafsam11@users.noreply.github.com>
2022-10-18 21:46:28 +0100

    Update main.c

--- test/main.c
+++ test/main.c
@@ -32,14 +32,14 @@ int main(void)
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
-	printf("String:[%s]\n", "I am a string !");
+	// printf("String:[%s]\n", "I am a string !");
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
+	// _printf("Unknown:[%r]\n");
 	_printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
 	printf("%i\n", _printf("%b\n", -10));
 	_printf("There is %b bytes in %b KB\n", 1024, 1);
