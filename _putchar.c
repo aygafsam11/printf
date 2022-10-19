bd22fc162b9019435362bb55d402197ba098dfeb
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-10-19 05:37:13 -0700

    updating files

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,21 @@
 #include <unistd.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string with newline
+ * @str: the string to print
+ *
+ * Return:( str-a)
+ */
+int _puts(char *str)
+{
+	char *a = str;/*declaration of variables*/
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print and printed
@@ -6,5 +23,15 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
