766e9103e596f2e2b5258eef0cf6847b57e3241d
LizzyDbosstechgirl <aladeelizabeth91@gmail.com>
2022-10-19 06:16:33 -0700

    Updating all files

--- _putchar.c
+++ _putchar.c
@@ -2,36 +2,11 @@
 #include "main.h"
 
 /**
- * _puts - prints a string with newline
- * @str: the string to print
- *
- * Return:( str-a)
- */
-int _puts(char *str)
-{
-	char *a = str;/*declaration of variables*/
-
-	while (*str)
-		_putchar(*str++);
-	return (str - a);
-}
-
-/**
  * _putchar - writes the character c to stdout
  * @c: the character to print and printed
  * Return: 0 on success and -1 error and errno is set appropiately
  */
 int _putchar(char c)
 {
-	static int i;
-	static char buf[OUTPUT_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+	return (write(1, &c, 1));
 }
